bf3e041a4a399c2605721906502abfbaa1ee3ce2
suraphop <suraphop@mail.com>
2025-08-29 14:01:48 +0700

    update create bat

---
+++ main.py
@@ -0,0 +1,5 @@
+from report_sale_amount import report_daily
+
+path = r"C:\Users\nmb_m\OneDrive\Desktop\python_office_2025-08-27\data\sales_data"
+
+report_daily(path)
---
+++ making_daily_report.bat
@@ -0,0 +1,1 @@
+conda activate python_office && python main.py
